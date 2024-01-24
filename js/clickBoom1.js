30f7a024265bc96b89650c222f1eb82d14e72f12
zhangyao <zhangyao220624@credithc.com>
2024-01-24 11:39:04 +0800

    Site updated: 2024-01-24 11:39:03

--- js/clickBoom1.js
+++ js/clickBoom1.js
@@ -1,1 +1,152 @@
-const numberOfParticules=20;const minOrbitRadius=50;const maxOrbitRadius=100;const minCircleRadius=10;const maxCircleRadius=20;const minAnimeDuration=900;const maxAnimeDuration=1500;const minDiffuseRadius=50;const maxDiffuseRadius=100;let canvasEl=document.querySelector(".fireworks");let ctx=canvasEl.getContext("2d");let pointerX=0;let pointerY=0;let tap="ontouchstart"in window||navigator.msMaxTouchPoints?"touchstart":"mousedown";let colors=["127, 180, 226","157, 209, 243","204, 229, 255"];function setCanvasSize(){canvasEl.width=window.innerWidth;canvasEl.height=window.innerHeight;canvasEl.style.width=window.innerWidth+"px";canvasEl.style.height=window.innerHeight+"px"}function updateCoords(t){pointerX=t.clientX||t.touches[0].clientX;pointerY=t.clientY||t.touches[0].clientY}function setParticuleDirection(t){let e=anime.random(0,360)*Math.PI/180;let n=anime.random(minDiffuseRadius,maxDiffuseRadius);let i=[-1,1][anime.random(0,1)]*n;return{x:t.x+i*Math.cos(e),y:t.y+i*Math.sin(e)}}function createParticule(t,e){let n={};n.x=t;n.y=e;n.color="rgba("+colors[anime.random(0,colors.length-1)]+","+anime.random(.2,.8)+")";n.radius=anime.random(minCircleRadius,maxCircleRadius);n.endPos=setParticuleDirection(n);n.draw=function(){ctx.beginPath();ctx.arc(n.x,n.y,n.radius,0,2*Math.PI,true);ctx.fillStyle=n.color;ctx.fill()};return n}function createCircle(t,e){let n={};n.x=t;n.y=e;n.color="#000";n.radius=.1;n.alpha=.5;n.lineWidth=6;n.draw=function(){ctx.globalAlpha=n.alpha;ctx.beginPath();ctx.arc(n.x,n.y,n.radius,0,2*Math.PI,true);ctx.lineWidth=n.lineWidth;ctx.strokeStyle=n.color;ctx.stroke();ctx.globalAlpha=1};return n}function renderParticule(e){for(let t=0;t<e.animatables.length;t++){e.animatables[t].target.draw()}}function animateParticules(e,n){let t=createCircle(e,n);let i=[];for(let t=0;t<numberOfParticules;t++){i.push(createParticule(e,n))}anime.timeline().add({targets:i,x:function(t){return t.endPos.x},y:function(t){return t.endPos.y},radius:.1,duration:anime.random(minAnimeDuration,maxAnimeDuration),easing:"easeOutExpo",update:renderParticule}).add({targets:t,radius:anime.random(minOrbitRadius,maxOrbitRadius),lineWidth:0,alpha:{value:0,easing:"linear",duration:anime.random(600,800)},duration:anime.random(1200,1800),easing:"easeOutExpo",update:renderParticule,offset:0})}let render=anime({duration:Infinity,update:function(){ctx.clearRect(0,0,canvasEl.width,canvasEl.height)}});document.addEventListener(tap,function(t){render.play();updateCoords(t);animateParticules(pointerX,pointerY)},false);setCanvasSize();window.addEventListener("resize",setCanvasSize,false);
+const numberOfParticules = 20;
+
+const minOrbitRadius = 50;
+const maxOrbitRadius = 100;
+
+const minCircleRadius = 10;
+const maxCircleRadius = 20;
+
+const minAnimeDuration = 900;
+const maxAnimeDuration = 1500;
+
+const minDiffuseRadius = 50;
+const maxDiffuseRadius = 100;
+
+let canvasEl = document.querySelector(".fireworks");
+let ctx = canvasEl.getContext("2d");
+let pointerX = 0;
+let pointerY = 0;
+
+let tap =
+  "ontouchstart" in window || navigator.msMaxTouchPoints
+    ? "touchstart"
+    : "mousedown";
+
+// sea blue
+let colors = ["127, 180, 226", "157, 209, 243", "204, 229, 255"];
+
+function setCanvasSize() {
+  canvasEl.width = window.innerWidth;
+  canvasEl.height = window.innerHeight;
+  canvasEl.style.width = window.innerWidth + "px";
+  canvasEl.style.height = window.innerHeight + "px";
+}
+
+function updateCoords(e) {
+  pointerX = e.clientX || e.touches[0].clientX;
+  pointerY = e.clientY || e.touches[0].clientY;
+}
+
+function setParticuleDirection(p) {
+  let angle = (anime.random(0, 360) * Math.PI) / 180;
+  let value = anime.random(minDiffuseRadius, maxDiffuseRadius);
+  let radius = [-1, 1][anime.random(0, 1)] * value;
+  return {
+    x: p.x + radius * Math.cos(angle),
+    y: p.y + radius * Math.sin(angle),
+  };
+}
+
+function createParticule(x, y) {
+  let p = {};
+  p.x = x;
+  p.y = y;
+  p.color =
+    "rgba(" +
+    colors[anime.random(0, colors.length - 1)] +
+    "," +
+    anime.random(0.2, 0.8) +
+    ")";
+  p.radius = anime.random(minCircleRadius, maxCircleRadius);
+  p.endPos = setParticuleDirection(p);
+  p.draw = function () {
+    ctx.beginPath();
+    ctx.arc(p.x, p.y, p.radius, 0, 2 * Math.PI, true);
+    ctx.fillStyle = p.color;
+    ctx.fill();
+  };
+  return p;
+}
+
+function createCircle(x, y) {
+  let p = {};
+  p.x = x;
+  p.y = y;
+  p.color = "#000";
+  p.radius = 0.1;
+  p.alpha = 0.5;
+  p.lineWidth = 6;
+  p.draw = function () {
+    ctx.globalAlpha = p.alpha;
+    ctx.beginPath();
+    ctx.arc(p.x, p.y, p.radius, 0, 2 * Math.PI, true);
+    ctx.lineWidth = p.lineWidth;
+    ctx.strokeStyle = p.color;
+    ctx.stroke();
+    ctx.globalAlpha = 1;
+  };
+  return p;
+}
+
+function renderParticule(anim) {
+  for (let i = 0; i < anim.animatables.length; i++) {
+    anim.animatables[i].target.draw();
+  }
+}
+
+function animateParticules(x, y) {
+  let circle = createCircle(x, y);
+  let particules = [];
+  for (let i = 0; i < numberOfParticules; i++) {
+    particules.push(createParticule(x, y));
+  }
+  anime
+    .timeline()
+    .add({
+      targets: particules,
+      x: function (p) {
+        return p.endPos.x;
+      },
+      y: function (p) {
+        return p.endPos.y;
+      },
+      radius: 0.1,
+      duration: anime.random(minAnimeDuration, maxAnimeDuration),
+      easing: "easeOutExpo",
+      update: renderParticule,
+    })
+    .add({
+      targets: circle,
+      radius: anime.random(minOrbitRadius, maxOrbitRadius),
+      lineWidth: 0,
+      alpha: {
+        value: 0,
+        easing: "linear",
+        duration: anime.random(600, 800),
+      },
+      duration: anime.random(1200, 1800),
+      easing: "easeOutExpo",
+      update: renderParticule,
+      offset: 0,
+    });
+}
+
+let render = anime({
+  duration: Infinity,
+  update: function () {
+    ctx.clearRect(0, 0, canvasEl.width, canvasEl.height);
+  },
+});
+
+document.addEventListener(
+  tap,
+  function (e) {
+    render.play();
+    updateCoords(e);
+    animateParticules(pointerX, pointerY);
+  },
+  false
+);
+
+setCanvasSize();
+window.addEventListener("resize", setCanvasSize, false);
--- js/snow2.js
+++ js/snow2.js
@@ -1,1 +1,49 @@
-(function(f){f.fn.snow=function(m){var r=f('<div id="snowbox" />').css({position:"absolute","z-index":"9999",top:"-50px"}).html("❄"),d=f(document).height(),c=f(document).width(),n={minSize:10,maxSize:20,newOn:1e3,flakeColor:"#AFDAEF"},m=f.extend({},n,m);var o=setInterval(function(){var n=Math.random()*c-100,o=.5+Math.random(),e=m.minSize+Math.random()*m.maxSize,t=d-200,i=n-500+Math.random()*500,a=d*10+Math.random()*5e3;r.clone().appendTo("body").css({left:n,opacity:o,"font-size":e,color:m.flakeColor}).animate({top:t,left:i,opacity:.2},a,"linear",function(){f(this).remove()})},m.newOn)}})(jQuery);$(function(){$.fn.snow({minSize:5,maxSize:50,newOn:300})});
+/*样式一*/
+(function ($) {
+    $.fn.snow = function (options) {
+        var $flake = $('<div id="snowbox" />').css({
+                'position': 'absolute',
+                'z-index': '9999',
+                'top': '-50px'
+            }).html('❄'),
+            documentHeight = $(document).height(),
+            documentWidth = $(document).width(),
+            defaults = {
+                minSize: 10,
+                maxSize: 20,
+                newOn: 1000,
+                flakeColor: "#AFDAEF" /* 此处可以定义雪花颜色，若要白色可以改为#FFFFFF */
+            },
+            options = $.extend({}, defaults, options);
+        var interval = setInterval(function () {
+            var startPositionLeft = Math.random() * documentWidth - 100,
+                startOpacity = 0.5 + Math.random(),
+                sizeFlake = options.minSize + Math.random() * options.maxSize,
+                endPositionTop = documentHeight - 200,
+                endPositionLeft = startPositionLeft - 500 + Math.random() * 500,
+                durationFall = documentHeight * 10 + Math.random() * 5000;
+            $flake.clone().appendTo('body').css({
+                left: startPositionLeft,
+                opacity: startOpacity,
+                'font-size': sizeFlake,
+                color: options.flakeColor
+            }).animate({
+                top: endPositionTop,
+                left: endPositionLeft,
+                opacity: 0.2
+            }, durationFall, 'linear', function () {
+                $(this).remove()
+            });
+        }, options.newOn);
+    };
+})(jQuery);
+$(function () {
+    $.fn.snow({
+        minSize: 5,
+        /* 定义雪花最小尺寸 */
+        maxSize: 50,
+        /* 定义雪花最大尺寸 */
+        newOn: 300 /* 定义密集程度，数字越小越密集 */
+    });
+});
+
